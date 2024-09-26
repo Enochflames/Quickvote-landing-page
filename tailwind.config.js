73aec350efe81d53d41ec9a3836d3ade506b4f2a
Enochflames <114030521+Enochflames@users.noreply.github.com>
2024-09-26 05:37:49 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+<style>
+    @import url('https://fonts.cdnfonts.com/css/kaisei-decol');
+</style>
 module.exports = {
   content: [],
   theme: {
     extend: {
+      fontFamily: {
+        kaisei: ['"Kaisei Decol"', 'sans-serif'],
+      },
     },
   },
   plugins: [],
 }
-
